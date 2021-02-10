05908eff29bb52184c1aa3a56bdcb86e147dcae1
Kari <kari_steinn@outlook.com>
2021-02-10 00:02:25 +0000

    Virkni komin og útlit fyrir form

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import { query } from './db.js';
+import { router as registrationRouter } from './registration.js';
+import { allowedNodeEnvironmentFlags } from 'process';
 
 dotenv.config();
 
@@ -7,9 +12,44 @@ const {
   PORT: port = 3000,
 } = process.env;
 
+let path = dirname(fileURLToPath(import.meta.url));
+path = path.substring(0, path.length -4);
+
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 // TODO setja upp rest af virkni!
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(join(path, './public')));
+
+/**
+ * Hjálparfall til að athuga hvort reitur sé gildur eða ekki.
+ *
+ * @param {string} field Middleware sem grípa á villur fyrir
+ * @param {array} errors Fylki af villum frá express-validator pakkanum
+ * @returns {boolean} `true` ef `field` er í `errors`, `false` annars
+ */
+function isInvalid(field, errors) {
+  return Boolean(errors.find(i => i.param === field));
+}
+
+app.locals.isInvalid = isInvalid;
+
+
+app.use('/', registrationRouter);
+// app.get('/', async (req, res) => {
+//   const title = 'Undirskriftarlisti';
+//   const result = await query('SELECT * from signatures;');
+//   console.log('result :>> ', result);
+
+//   res.render('index', { title });
+// });
+
+// app.post('/post', (req, res) => {
+//   res.send(`POST gögn: ${JSON.stringify(req.body)}`);
+// });
 
 // Verðum að setja bara *port* svo virki á heroku
 app.listen(port, () => {
--- src/registration.js
+++ src/registration.js
@@ -1,3 +1,158 @@
 import express from 'express';
+import { query, select, insert } from './db.js';
+import xss from 'xss';
+import { check, validationResult, sanitize } from 'express-validator';
 
 // TODO skráningar virkni
+
+export const router = express.Router();
+
+// Fylki af öllum validations fyrir undirskrift
+const validations = [
+  check('name')
+    .isLength({ min: 1 })
+    .withMessage('Nafn má ekki vera tómt'),
+
+  check('name')
+    .isLength({ max: 128 })
+    .withMessage('Nafn má að hámarki vera 128 stafir'),
+
+  check('nationalId')
+    .isLength({ min: 1 })
+    .withMessage('Kennitala má ekki vera tóm'),
+
+  check('nationalId')
+    .matches(/^[0-9]{6}-?[0-9]{4}$/)
+    .withMessage('Kennitala verður að vera á formi 000000-0000 eða 0000000000'),
+
+  check('comment')
+    .isLength({ max: 400 })
+    .withMessage('Athugasemd má að hámarki vera 400 stafir')
+];
+
+// Fylki af öllum hreinsunum fyrir umsókn
+const sanitazions = [
+  check('name').trim().escape(),
+  sanitizeXss('name'),
+
+  sanitizeXss('nationalId'),
+  check('nationalId')
+    .trim().blacklist('-').escape()
+    .toInt(),
+
+  sanitizeXss('comment'),
+  check('comment').trim().escape(),
+];
+
+/**
+ * Higher-order fall sem umlykur async middleware með villumeðhöndlun.
+ *
+ * @param {function} fn Middleware sem grípa á villur fyrir
+ * @returns {function} Middleware með villumeðhöndlun
+ */
+function catchErrors(fn) {
+  return (req, res, next) => fn(req, res, next).catch(next);
+}
+
+/**
+ * Hjálparfall sem XSS hreinsar reit í formi eftir heiti.
+ *
+ * @param {string} fieldName Heiti á reit
+ * @returns {function} Middleware sem hreinsar reit ef hann finnst
+ */
+function sanitizeXss(fieldName) {
+  return (req, res, next) => {
+    if (!req.body) {
+      next();
+    }
+
+    const field = req.body[fieldName];
+
+    if (field) {
+      req.body[fieldName] = xss(field);
+      console.log(req.body[fieldName]);
+    }
+
+    next();
+  };
+}
+
+async function registrationGet(req, res) {
+  const data = {
+    title: 'Undirskriftarlisti',
+    name: '',
+    nationalId: '',
+    comment: '',
+    anonymous: '',
+    date: '',
+    errortitle: '',
+    errors: [],
+  };
+  const result = await query('SELECT * from signatures;');
+  const rows = result.rows;
+
+  res.render('index', { data, rows });
+}
+
+async function showErrors(req, res, next) {
+  const {
+    body: {
+      title = 'Undirskriftarlisti',
+      name = '',
+      nationalId = '',
+      comment = '',
+      anonymous = false,
+    } = {},
+  } = req;
+
+  const data = {
+    title,
+    name,
+    nationalId,
+    comment,
+    anonymous,
+  };
+
+  const validation = validationResult(req);
+
+  if (!validation.isEmpty()) {
+    const errors = validation.array();
+    data.title = 'Undirskriftarlisti';
+    data.errors = errors;
+    data.errortitle = 'Villur við undirskrift:';
+
+    const result = await query('SELECT * from signatures;');
+    const rows = result.rows;
+
+    return res.render('index', { data, rows });
+  }
+  return next();
+}
+
+async function registrationPost(req, res) {
+  const {
+    body: {
+      name = '',
+      nationalId = '',
+      comment = '',
+      anonymous = false,
+    } = {},
+  } = req;
+
+  const data = {
+    name,
+    nationalId,
+    comment,
+    anonymous,
+  };
+
+  await insert(data);
+  registrationGet(req, res);
+}
+
+router.get('/', registrationGet);
+router.post('/', validations,
+  showErrors,
+  sanitazions,
+  catchErrors(registrationPost)
+);
